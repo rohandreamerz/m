b4fc741488fa5932acfe3ab2e2b06c4280f6cc15
Rohan Kamble <57754104+rohandreamerz@users.noreply.github.com>
2024-06-08 17:14:04 +0000

    done

--- app.py
+++ app.py
@@ -4,51 +4,29 @@ from streamlit_lottie import st_lottie
 import json 
 from PIL import Image
 import time
+import pandas as pd
+import plotly.express as px
 
-
 st.balloons()
 time.sleep(1)
 st.balloons()
 time.sleep(1)
 st.balloons()
-time.sleep(1)
-st.snow()
-time.sleep(1.3)
-st.snow()
-time.sleep(1.3)
-st.snow()
-time.sleep(1.3)
-st.snow()
-time.sleep(1)
-st.snow()
+time.sleep(2)
 
 file_path = __file__
 dir_name = os.path.dirname(file_path)
 lottie_url= os.path.join(dir_name, "happy_bday.json")
 
-image_path = os.path.join(dir_name, "m_collage.jpg")
-image = Image.open(image_path)
+st.markdown("<h1 style='text-align: center; color: #FF3C00;'>Hi Sapnaa...</h1>", unsafe_allow_html=True)
 
-lottie_url_1 = os.path.join(dir_name, "bday_anim.json")
 with open(lottie_url, "r") as f:
         lottie_json = json.load(f)
 
 if lottie_json is not None:
         st_lottie(lottie_json)
 else:
-        st.error("Failed to load animation")
+        pass
 
-
-with open(lottie_url_1, "r") as f:
-    lottie_json = json.load(f)
-
-if lottie_json is not None:
-    container = st.container()
-    with container:
-        st_lottie(lottie_json)
-        st.image(image, use_column_width=True)
-else:
-    st.error("Failed to load animation")
-    
-
-st.text('Happy Birthday Manmitha!!! 🥳🎂🎊 Wishing you all the best on your special day.\nMay your year ahead be filled with love, joy, and prosperity.\nKeep smiling 😊 and spreading your positivity to everyone around you🌈.\nCheers 🥂 to a fantastic year ahead!')
+time.sleep(3)
+st.markdown("<p style='font-family:cursive;font-size:30px;text-align: center; color: black;'>May all your Dreams Come True!🌈.<br>Cheers 🥂 to a fantastic year ahead!<br>-----------<br>Yours Truly,<br>Rohan</p>",unsafe_allow_html=True)
